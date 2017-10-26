6d55412f7b26b37a5d90f327e71470a7273d6f6b
coolkk7 <33130911+coolkk7@users.noreply.github.com>
2017-10-26 22:52:49 +0530

    initial configuration

---
+++ example.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-8c1be5f6"
+  instance_type = "t2.micro"
+  key_name      = "kkb-Linux-keypair"
+  tags {
+    Name  = "kkb-terraform-test"
+    Owner = "KKB"
+  }
+}
